7c3196242ba7691f5a7fcf95227b81450adcb3d5
Dany <Dany@MacBook-Air-de-Mac.local>
2017-04-03 21:42:56 -0500

    maquetado basico

--- intro_ux/landing/js/landing.js
+++ intro_ux/landing/js/landing.js
@@ -1,7 +1,34 @@
 $(document).ready(function(){
-
+$('.imgLiquidNoFill').imgLiquid({
+    fill: false
+})
    // Escala y Recorta la imagen para que quepa exacto en su contenedor
    $('.imgLiquidFill').imgLiquid()
+
+
+$('.v-center').each(function() {
+
+  elementoPadre = $(this)
+
+  elementosHijos = elementoPadre.children()
+
+  alturaHijos = 0
+
+  elementosHijos.each(function() {
+    elementoHijo = $(this)
+
+    alturaHijos += elementoHijo.height()
+  })
+
+  diferencia = elementoPadre.height()-alturaHijos
+
+  espacioSuperior = diferencia / 2
+
+  elementoPadre.css({
+    paddingTop: espacioSuperior
+  })
+
+})
 
    console.log('Landing Lista')
 
